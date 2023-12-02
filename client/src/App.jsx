998c41e2b4bd186d982366bc87b36c43ea07cb09
Robin Kelsick II <robinkelsick@gmail.com>
2023-12-01 21:59:13 -0500

    Add background image and update navigation menu styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import NavMenu from './components/NavMenu';
+import galaxy from './assets/galaxy.jpg';
 
 function App() {
   return (
-    <div className='w-full h-screen'>
+    <div
+      className='w-full h-screen m-0 p-0 bg-no-repeat bg-cover bg-center'
+      style={{ backgroundImage: `url(${galaxy})` }}
+    >
       <NavMenu />
     </div>
   );
