c2581c6dd4083b1fc3c2292482e14091e36a1b49
Robin Kelsick II <robinkelsick@gmail.com>
2023-12-01 19:38:31 -0500

    initial commit

---
+++ client/postcss.config.js
@@ -0,0 +1,9 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const plugins = {
+  tailwindcss,
+  autoprefixer,
+};
+
+export default plugins;
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p className='text-lg'>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
